7d280767f5785e931a71bac5f0dd17e45ec84d68
DYNELLE BENITA FERNANDES 160905027 <dynelle.benita@learner.manipal.edu>
2019-10-29 09:19:08 +0530

    DB added

---
+++ dbo.Booking.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Booking]
+(
+	[BkID] INT NOT NULL PRIMARY KEY, 
+    [IDBus] INT NOT NULL, 
+    [Nameuser] VARCHAR(50) NOT NULL, 
+    CONSTRAINT [Bus_ID] FOREIGN KEY ([IDBus]) REFERENCES [Bus]([BusID]), 
+    CONSTRAINT [User_name] FOREIGN KEY ([Nameuser]) REFERENCES [User]([Username]) 
+)
