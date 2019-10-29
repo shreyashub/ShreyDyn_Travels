9be5ebb2009b3cb74598069f163f15473b69683f
DYNELLE BENITA FERNANDES 160905027 <dynelle.benita@learner.manipal.edu>
2019-10-29 09:25:08 +0530

    Users added

---
+++ dbo.User.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Username] VARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Password] VARCHAR(50) NOT NULL, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Email] VARCHAR(50) NOT NULL
+)
